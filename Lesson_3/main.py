82069c8f7d9d2de8731e583664852d4fdb459cd3
KU3MI4 <kuzmichov@bk.ru>
2023-07-27 12:12:54 +0300

    add lesson 3

---
+++ Lesson_3/main.py
@@ -0,0 +1,88 @@
+# def sum_nunbers(n, y = 'Hello'):
+#     print(y)
+#     summa = 0
+#     for i in range(1, n+1):
+#         summa += i
+#     return summa
+
+# a = sum_nunbers(5, 'qwert')    
+# print(a)
+
+# def sum_str(*args):
+#     res = ''
+#     for i in args:
+#         res += str(i)
+#     return res
+
+# print(sum_str('q', 'e', 'l'))
+# print(sum_str('q', 'e', 'l', 'r', 'f'))
+# print(sum_str(1, 8, 9))
+
+# import modul1
+
+# print(modul1.max1(5, 9))
+
+
+# from modul1 import max1
+
+# print(max1(10, 9))
+
+
+# import modul1 as m1
+
+# print(m1.max1(10, 19))
+
+
+# def fib(n):
+#     if n in [1, 2]:
+#         return 1
+#     return fib(n-1) + fib(n-2)
+
+# list_1 = []
+# for i in range(1, 10):
+#     list_1.append(fib(i))
+# print(list_1)
+
+
+# def quick_sort(array):
+#     if len(array) <= 1:
+#         return array
+#     else:
+#         pivot = array[0]
+#     less = [i for i in array[1:] if i <= pivot]
+#     greater = [i for i in array[1:] if i > pivot]
+#     return quick_sort(less) + [pivot] + quick_sort(greater)
+
+# print(quick_sort([10, 5, 2]))
+
+
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+            
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+            
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+            
+list1 = [12,23,5,25,6,87,8,3,1]
+merge_sort(list1)
+print(list1)
